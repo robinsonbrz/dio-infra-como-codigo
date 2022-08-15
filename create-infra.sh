d9f7104857105bd014121e08f1a8b46ba0df1f5f
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-08-15 15:47:03 -0300

    criação de usuarios

--- create-infra.sh
+++ create-infra.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #Diretorios
 mkdir /publico
 mkdir /adm
@@ -10,6 +11,17 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-
 # usuarios
+useradd carlos -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd maria -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd joao -m -s /bin/bash $(openssl passd -crypt Senha123)
+
+useradd debora -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd sebastiana -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd roberto -m -s /bin/bash $(openssl passd -crypt Senha123)
+
+useradd josefina -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd amanda -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd rogerio -m -s /bin/bash $(openssl passd -crypt Senha123)
+
 
