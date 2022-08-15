9b69b83942eb7c3c3c64799bb11a5aab46988ee6
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-08-15 15:48:36 -0300

    bind users e groups

--- create-infra.sh
+++ create-infra.sh
@@ -12,16 +12,16 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 # usuarios
-useradd carlos -m -s /bin/bash $(openssl passd -crypt Senha123)
-useradd maria -m -s /bin/bash $(openssl passd -crypt Senha123)
-useradd joao -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd carlos -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash $(openssl passd -crypt Senha123)
-useradd sebastiana -m -s /bin/bash $(openssl passd -crypt Senha123)
-useradd roberto -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd debora -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash $(openssl passd -crypt Senha123)
-useradd amanda -m -s /bin/bash $(openssl passd -crypt Senha123)
-useradd rogerio -m -s /bin/bash $(openssl passd -crypt Senha123)
+useradd josefina -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash $(openssl passd -crypt Senha123) -G GRP_SEC
 
 
